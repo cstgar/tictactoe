57404388e349cc398e6eaecd5726b38b249a81a8
cstgar <cinthiakstg@gmail.com>
2024-09-24 10:20:20 -0600

    adding home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TableroComponent } from './tablero/tablero.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        //string en vacio para poder dejar el componente como la ruta principal
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'partida',
+        component: TableroComponent
+    }
+];
